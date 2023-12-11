336eda71a08dc3ebcdd9da4f0350238ffb9904a2
Qulxizer <qulxizer@gmail.com>
2023-12-11 14:53:59 +0300

    Update

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 - [x] Making Greeting for the game.
 - [x] Getting input from the user.
 - [x] Get random number using [rand](https://github.com/rust-random/rand).
-- [ ] Comparing the guess to the secret number.
-- [ ] returning if the number is greater or less than the secret number.
+- [x] Comparing the guess to the secret number.
+- [x] returning if the number is greater or less than the secret number.
 - [ ] compile everything to windows, mac and linux.
 
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     // Greeting
     println!("Welcome to Guess the number!");
-    println!("please enter your guess: ");
-
+    
     // Generating Secret number
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The Secret number is {secret_number}");
-
-    // Storing Guess
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("Your guess is {guess}:");
+    
+    loop {
+        // Storing Guess
+        println!("please enter your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess:u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Try a bigger number!"),
+            Ordering::Greater => println!("Try a smaller!"),
+            Ordering::Equal => {
+                println!("That's right!");
+                break;
+            }
+    }
+    };
     
 }
